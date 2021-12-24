0bc5650ce8cdb57b8d4aceb6210430fe2ce6347a
Shashank <shashanke7y@gmail.com>
2021-12-23 19:28:17 -0500

    firebase setup

---
+++ src/firebase/init.tsx
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD7rL3NqYt1YRVtvvgofabgZQqa24s-pmQ",
+  authDomain: "lobbydemos.firebaseapp.com",
+  projectId: "lobbydemos",
+  storageBucket: "lobbydemos.appspot.com",
+  messagingSenderId: "544423438814",
+  appId: "1:544423438814:web:d7359709e785be67f4554f",
+  measurementId: "G-T8GCYTPEPG",
+};
+
+initializeApp(firebaseConfig);
