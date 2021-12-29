4cf9128487c869c0cdcf32c21f892fe21f2e5779
listentothefrog <shashanke7y@gmail.com>
2021-12-29 15:12:53 -0500

    export auth variable

--- src/firebase/init.ts
+++ src/firebase/init.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD7rL3NqYt1YRVtvvgofabgZQqa24s-pmQ",
@@ -9,5 +10,7 @@ const firebaseConfig = {
   appId: "1:544423438814:web:d7359709e785be67f4554f",
   measurementId: "G-T8GCYTPEPG",
 };
+
+export const auth = getAuth();
 
 initializeApp(firebaseConfig);
