ee9a9feaff2ad96ddce0a58fec379f75366f49e0
listentothefrog <shashanke7y@gmail.com>
2021-12-30 11:57:14 -0500

    add: sign out button

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -7,6 +7,7 @@ const Chat = () => {
       {auth.currentUser?.displayName}
       {auth.currentUser?.email}
       <img src={auth.currentUser?.photoURL! as string} />
+      <button onClick={() => auth.signOut()}>Sign Out</button>
     </div>
   );
 };
