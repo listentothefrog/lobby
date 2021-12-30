d62a63423f1ddf456c8062eff758a7286b93d7aa
listentothefrog <shashanke7y@gmail.com>
2021-12-30 11:54:39 -0500

    displays diffrent screen if user is logged in

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React, { Suspense } from "react";
 import Login from "./components/Login/Login";
+import { auth } from "./firebase/init";
+import { useAuthState } from "react-firebase-hooks/auth";
+import Chat from "./components/Chat/Chat";
 
 function App() {
+  const [user] = useAuthState(auth);
   return (
-    <Suspense fallback="Loading...">
-      <Login />
-    </Suspense>
+    <Suspense fallback="Loading...">{user ? <Chat /> : <Login />}</Suspense>
   );
 }
 
