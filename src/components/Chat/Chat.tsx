b4ed74ef12d26338027408273207d88f53d8bcf7
listentothefrog <shashanke7y@gmail.com>
2022-01-01 14:26:52 -0500

    users can read messages in realtime

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,11 +1,30 @@
-import { addDoc, collection, serverTimestamp } from "@firebase/firestore";
+import {
+  addDoc,
+  collection,
+  query,
+  serverTimestamp,
+} from "@firebase/firestore";
+import { onSnapshot, orderBy } from "firebase/firestore";
 import React, { useState } from "react";
+import { useEffect } from "react";
 import { auth, db } from "src/firebase/init";
 import "./Chat.css";
 
 const Chat = () => {
   const [text, setText] = useState("");
+  const [messages, setMessages] = useState([]);
   const messagesRef = collection(db, "messages");
+  const q = query(messagesRef, orderBy("createdAt"));
+  useEffect(() => {
+    onSnapshot(q, (querySnapshot: any) => {
+      let messages: any = [];
+      querySnapshot.forEach((doc: any) => {
+        messages.push(doc.data());
+      });
+      setMessages(messages);
+    });
+  }, []);
+
   const sendMessage = async (e: any) => {
     e.preventDefault();
     await addDoc(messagesRef, {
@@ -18,7 +37,11 @@ const Chat = () => {
   };
   return (
     <>
-      <main></main>
+      <main>
+        {messages.map((msg: any) => (
+          <div key={msg.id}>{msg.text}</div>
+        ))}
+      </main>
       <form onSubmit={sendMessage}>
         <input
           onChange={(e) => setText(e.target.value)}
