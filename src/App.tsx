b29cf36cafcdb346e718247b12ea6b5a90846a71
Shashank <shashanke7y@gmail.com>
2022-01-10 10:30:05 -0500

    useEffect that checks if the user is logged in

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
-import React, { Suspense } from "react";
+import React, { useEffect } from "react";
 import Login from "./components/Login/Login";
 import { auth } from "./firebase/init";
 import { useAuthState } from "react-firebase-hooks/auth";
 import Chat from "./components/Chat/Chat";
+import { Routes, Route, useNavigate } from "react-router-dom";
 
 function App() {
+  const navigate = useNavigate();
   const [user] = useAuthState(auth);
+  useEffect(() => {
+    if (user) {
+      navigate("/lobby");
+    }
+    if (!user) {
+      navigate("/");
+    }
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <Suspense fallback="Loading...">{user ? <Chat /> : <Login />}</Suspense>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/lobby" element={<Chat />} />
+        </Routes>
       </header>
     </div>
   );
