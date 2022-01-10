ba971a14509b862aaf919411c632c8d4a43665e5
Shashank <shashanke7y@gmail.com>
2022-01-10 11:15:29 -0500

    redirect user to / route

--- src/components/Account/Account.tsx
+++ src/components/Account/Account.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { auth } from "src/firebase/init";
 
 const Account = () => {
+  const navigate = useNavigate();
+  let authToken = sessionStorage.getItem("Auth Token");
+  useEffect(() => {
+    if (!authToken) {
+      navigate("/");
+    }
+  }, []);
   return <div>{auth.currentUser?.displayName}</div>;
 };
 
